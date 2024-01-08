d64e70f9ca75b7d99b22696229b2029f984cd68d
Omar El Houmadi <oel-houm@e1r8p7.1337.ma>
2024-01-08 19:24:33 +0100

    .

--- helpers/display_debug_infos.c
+++ helpers/display_debug_infos.c
@@ -6,7 +6,7 @@
 /*   By: oel-houm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 03:33:23 by oel-houm          #+#    #+#             */
-/*   Updated: 2024/01/02 03:33:44 by oel-houm         ###   ########.fr       */
+/*   Updated: 2024/01/08 18:46:20 by oel-houm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,35 +18,34 @@
 
 void    show_info(t_map_info *map_data)
 {
-    printf("\n");
-    int index = map_data->texture_start_index;
-    while (index <= map_data->map_end_index)
-    {
-        printf("i=%d=%s\n", index + 0, map_data->map_content[index]);
-        index++;
-    }
-    printf("\n");
-    printf("textures start at index: %d\n", map_data->texture_start_index);
-    printf("textures end at index: %d\n", map_data->texture_end_index);
-    printf("map start at index: %d\n", map_data->map_start_index);
-    printf("map end at index: %d\n", map_data->map_end_index);
-    printf("map eof index: %d\n", map_data->eof_index);
-
-    /* display extracted fc data: */
-    
-    // printf("\n\n");
-    // printf("ceilling values are:\t%d,%d,%d\n", map_data->c_values[0], map_data->c_values[1], map_data->c_values[2]);
-    // printf("ceilling color is:\t%d", map_data->c_color);
-    // printf("\n");
-    // printf("floor values are:\t%d,%d,%d\n", map_data->f_values[0], map_data->f_values[1], map_data->f_values[2]);
-    // printf("floor color is:\t\t%d", map_data->f_color);
-
-    //index 0 to first line texture
-    // printf("%d -> %d\n", 0+1, map_data->texture_start_index+1);
-    // last line texture to first line map
-    // printf("%d -> %d\n", map_data->texture_end_index+1, map_data->map_start_index+1);
-    // last line map to eof index
-    // printf("%d -> %d\n", map_data->map_end_index+1, map_data->eof_index+1);
-    
-    // printf("\n ==> parsing successful <==\n");
+	printf("\n");
+	int index = map_data->texture_start_index;
+	while (index <= map_data->map_end_index)
+	{
+		printf("i=%d=%s\n", index + 0, map_data->map_content[index]);
+		index++;
+	}
+	printf("\n");
+	printf("textures start at index: %d\n", map_data->texture_start_index);
+	printf("textures end at index: %d\n", map_data->texture_end_index);
+	printf("map start at index: %d\n", map_data->map_start_index);
+	printf("map end at index: %d\n", map_data->map_end_index);
+	printf("map eof index: %d\n", map_data->eof_index);
+	
+	/* display extracted fc data: */
+	// printf("\n\n");
+	// // printf("ceilling values are:\t%d,%d,%d\n", map_data->c_values[0], map_data->c_values[1], map_data->c_values[2]);
+	// printf("ceilling color is:\t%d", map_data->c_color);
+	// printf("\n");
+	// printf("floor values are:\t%d,%d,%d\n", map_data->f_values[0], map_data->f_values[1], map_data->f_values[2]);
+	// printf("floor color is:\t\t%d", map_data->f_color);
+	//
+	// //index 0 to first line texture
+	// printf("%d -> %d\n", 0+1, map_data->texture_start_index+1);
+	// last line texture to first line map;
+	// printf("%d -> %d\n", map_data->texture_end_index+1, map_data->map_start_index+1);
+	// last line map to eof index
+	// printf("%d -> %d\n", map_data->map_end_index+1, map_data->eof_index+1);
+	//
+	// // printf("\n ==> parsing successful <==\n");
 }
--- helpers/remove_trailing_newline.c
+++ helpers/remove_trailing_newline.c
@@ -6,17 +6,17 @@
 /*   By: oel-houm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 03:32:46 by oel-houm          #+#    #+#             */
-/*   Updated: 2024/01/02 03:33:02 by oel-houm         ###   ########.fr       */
+/*   Updated: 2024/01/08 18:44:24 by oel-houm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../parsing.h"
 
-void remove_trailing_newline(char *str)
+void	remove_trailing_newline(char *str)
 {
-    int len;
+	int	len;
 
-    len = ft_strlen(str);
-    if (len > 0 && str[len - 1] == '\n')
-        str[len - 1] = '\0';
+	len = ft_strlen(str);
+	if (len > 0 && str[len - 1] == '\n')
+		str[len - 1] = '\0';
 }
